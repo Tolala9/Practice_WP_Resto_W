fa1bd30cbc15cddccbfb2f3406f8d9f2cf515023
Tolala9 <ly.an.mode1@gmail.com>
2018-03-07 16:06:35 +0200

    Edit Menu

--- template-parts/content.php
+++ template-parts/content.php
@@ -53,7 +53,7 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+	<!-- <footer class="entry-footer">
 		<?php resto_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</footer><!-- .entry-footer -->  
 </article><!-- #post-<?php the_ID(); ?> -->
